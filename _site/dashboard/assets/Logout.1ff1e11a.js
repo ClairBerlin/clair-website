985b3ece4399987d0ba07f4cf2c74577e6dc85ab
Jan Weil <jan.weil@decarbon1ze.com>
2022-01-15 22:10:04 +0100

    Update como UI

---
+++ _site/dashboard/assets/Logout.1ff1e11a.js
@@ -0,0 +1,1 @@
+import{u as a,b1 as n,D as r,o as c,f as i,b as l,t as u}from"./vendor.5ded90fb.js";const f={async setup(p){let o,t;const e=a();return[o,t]=n(()=>e.dispatch("authuser/logout")),await o,t(),r.remove("csrftoken"),window.location.href=window.location.origin+"/accounts/login/",(s,d)=>(c(),i("div",null,[l("h1",null,u(s.$t("logging-out")),1)]))}};export{f as default};
