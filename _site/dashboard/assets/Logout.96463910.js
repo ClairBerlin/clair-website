bf70ac9cff8ef6afd0fcabbf9d5703a6954de9d4
Ulrich Schuster <ulrich.schuster@koing.de>
2022-05-14 08:51:46 +0200

    Update como UI

--- _site/dashboard/assets/Logout.96463910.js
+++
@@ -1,1 +0,0 @@
-import{u as s,b5 as n,E as r,o as c,f as i,b as l,t as u}from"./vendor.c632a9d3.js";const d={async setup(p){let t,o;const e=s();return[t,o]=n(()=>e.dispatch("authuser/logout")),await t,o(),r.remove("csrftoken"),window.location.href=window.location.origin+"/accounts/login/",(a,_)=>(c(),i("div",null,[l("h1",null,u(a.$t("logging-out")),1)]))}};export{d as default};
